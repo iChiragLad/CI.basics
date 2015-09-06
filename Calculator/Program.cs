19b3aaf75f0e161f8e28980210f38090bb23de75
chirag <chiragladn92@gmail.com>
2015-09-07 02:05:30 +0530

    calculator functionality added

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,11 +5,39 @@ using System.Text;
 
 namespace Calculator
 {
+    class Maths
+    {
+        public int Addition(int a, int b)
+        {
+            return(a+b);
+        }
+        public int Substarction(int a, int b)
+        {
+            return (a - b);
+        }
+        public int Multiplication(int a, int b)
+        {
+            return (a * b);
+
+        }
+        public int Division(int a, int b)
+        {
+            return (a / b);
+        }
+
+        public void display(int result)
+        {
+            Console.WriteLine("Answer is : {0}", result);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Maths m_Maths = new Maths();
+            int result = m_Maths.Addition(10, 20);
+            m_Maths.display(result);
+
             Console.ReadLine();
         }
     }
