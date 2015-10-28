1051506ff317d70ca9b6b90a42b8d598bee8976a
unknown <chirag_lad@PTD09704.persistent.co.in>
2015-10-28 19:10:32 +0530

    First commit from office

---
+++ Calculator/Logic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Logic
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Substarction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        public void display(int result)
+        {
+            Console.WriteLine("Answer is : {0}", result);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic m_Maths = new Logic();
+            int result = m_Maths.Addition(10, 20);
+            m_Maths.display(result);
+
+            Console.ReadLine();
+        }
+    }
+}
